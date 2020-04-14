b42a4db7b8f2b521998e8639fae42351c5c30a8e
manhsy <manhsy@192.168.0.11>
2020-04-14 15:52:08 -0700

    commit and push by Manh

--- Lab02/Hello.java
+++ Lab02/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("219333232 - This line was added by Jason Tuyen.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("219386155 - This line was added by Manh Sy.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
