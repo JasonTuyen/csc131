f7d7a1b4b867f65c91ad408667bd293f6d67dece
Ramon <Ramon@192.168.86.21>
2020-04-17 16:56:27 -0700

    Commit and Push by Ramon

--- Lab02/Hello.java
+++ Lab02/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 		// Add a new line with your information
 		System.out.println("219333232 - This line was added by Jason Tuyen.");
 		System.out.println("219386155 - This line was added by Manh Sy.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("216546786 - This line was added by Ramon Mata.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
