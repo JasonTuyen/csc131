163ced4f592e121ee867a1e3df0dba41aec52dfe
science-sean <spstrachan@csus.edu>
2020-04-23 12:29:29 -0700

    Created: Question class file
    Created a Question class file and added some initial method headers.

---
+++ Question.java
@@ -0,0 +1,20 @@
+package quizApp;
+
+protected class Question {
+
+    // members
+    private Category category;
+    private String   prompt;
+
+    // Constructor
+    Question( Category category, String prompt ) {
+    }
+
+    // getters only, no setters for this class
+    public Category category() {
+    }
+
+    public Strong prompt() {
+    }
+
+} // Question class
