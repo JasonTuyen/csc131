2dc9b910ace86c1dd0fa263435dbe7d89bd10c69
science-sean <spstrachan@csus.edu>
2020-04-23 16:05:23 -0700

    Added: Category and annotations
    Added the Category class and annotations about how some things could be done in
    the Quiz class.

--- Test.java
+++ Test.java
@@ -6,6 +6,32 @@ protected class Quiz {
 
     // members
     ArrayList<Question> questions;
+    ArrayList<Category> categories;  // should this be a hashmap? idk
 
+    // Constructors
+
+    // methods
+    // implement an iterator here:
+    /*
+      The initializer should create an array of the same size as questions
+      It should add all the idices 0..(length of questions) and then shuffle them
+      Start at 0 in this new array and return the Question  at the given index
+      to return all questions in randomized order
+     */
+    // adding a question should be private or handled by constructor alone
+    // 
 
 } // Quiz class
+
+protected class Category {
+
+    // members
+    final int    id;
+    final String name;
+    final String description;
+    final String positiveName;
+    final String positiveDescription;
+    final String negativeName;
+    final String negativeDescription;
+
+} // Category class
