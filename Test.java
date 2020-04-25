f51e53101714bd62731a054039cad506c5732ff0
science-sean <spstrachan@csus.edu>
2020-04-25 10:58:23 -0700

    Added iterator to Test
    Added an iterator to the Test class which randomizes the Question order without
    changing the actual question list.

--- Test.java
+++ Test.java
@@ -1,15 +1,18 @@
 package quizApp;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.io.File;
+import java.util.Collections;
 
-protected class Quiz {
+protected class Quiz implements Iterable<Question> {
 
     // members
     ArrayList<Question> questions;
     ArrayList<Category> categories;  // should this be a hashmap? idk
 
     // Constructors
-    Quiz(){
+    public Quiz( File questionFile, File categoryFile ){
         
     }
 
@@ -21,8 +24,32 @@ protected class Quiz {
       Start at 0 in this new array and return the Question  at the given index
       to return all questions in randomized order
      */
+    private class QuestionIterator implements Iterator<> {
+
+        private       int                 curr;
+        private final ArrayList<Question> shuffledQuestions;
+
+        public QuestionIterator() {
+            curr = 0;  // should already be 0 but make this explicit
+
+            // Shallow copy the questions list and shuffle the copy
+            shuffledQuestions = new ArrayList<Questions>(questions);
+            Collections.shuffle(shuffledQuestions);
+        }
+
+        public boolean hasNext() {
+            return curr < shuffledQuestions.size();
+        }
+
+        public Question next() {
+            Question item = shuffledQuestions.get(curr);
+            curr++;
+            return item;
+        }
+
+    } // QuestionIterator class
+
     // adding a question should be private or handled by constructor alone
-    // 
 
 } // Quiz class
 
@@ -48,7 +75,7 @@ protected class Category {
         }
         catch(Exception e){
             System.out.println("File does not exist.");
-        }    
+        }
     }
 
 } // Category class
