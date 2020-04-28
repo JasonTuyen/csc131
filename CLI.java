b8de968dcd14f6ca9b2172bd51b841babd3f9491
science-sean <spstrachan@csus.edu>
2020-04-27 17:25:53 -0700

    Added a Command Line Interface for running the Quiz
    The interface is pretty rough but it at least lets us test all the other
    class constructors. I have it here more to give us a blueprint to follow when
    we write the back end for the GUI in the next sprint.

---
+++ CLI.java
@@ -0,0 +1,65 @@
+// package quizApp;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class CLI {
+
+    public static void runQuiz(Quiz quiz) {
+        System.out.println("Welcome to the Quiz App by Hornet Corp!\n");
+        System.out.println(
+                "For each of the following questions or statements please enter a number from 1 to 9 when 1 is Strong Disagree and 9 is Strongly Agree.");
+
+        int[] catScores = new int[quiz.categories.size()];
+        Scanner userIn = new Scanner(System.in);
+        int choice;
+
+        for (Question q : quiz) {
+            System.out.println(q.categoryId() + q.prompt());
+            do {
+                System.out.print("> ");
+                choice = userIn.nextInt();
+            } while (choice < 1 || choice > 9);
+            choice -= 5;
+            catScores[Math.abs(q.categoryId()) - 1] += Math.signum(q.categoryId()) * choice;
+            System.out.println();
+        }
+
+        int overallResult = 0;
+        for (int i = 0; i < quiz.categories.size(); i++) {
+            overallResult += ((int)Math.pow(2,i)) * ((catScores[i] > 0) ? (0) : (1));
+        }
+        System.out.format("The result of your quiz is: %s!\n\n", quiz.results[overallResult]);
+        int i = 0;
+        for (Category c: quiz.categories) {
+            System.out.format("Category %d is %s.", i+1, c.name);
+            System.out.println(c.description);
+            if(catScores[i] == 0) {
+                System.out.println("Your test results show that you do not have a strong preference in this category.");
+            } else if (catScores[i] > 0) {
+                System.out.format("Your test results show that you prefer %s!\n", c.positiveName);
+                System.out.println(c.positiveDescription);
+            } else {
+                System.out.format("Your test results show that you prefer %s!\n", c.negativeName);
+                System.out.println(c.negativeDescription);
+            }
+            System.out.println();
+            i++;
+        }
+
+        userIn.close();
+    }
+
+} // CLI class
+
+class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        File questionFile = new File("ProgLanguageQuiz.txt");
+        File categoryFile = new File("ProgLanguageCategories.txt");
+
+        Quiz progLangQuiz = new Quiz(questionFile, categoryFile);
+
+        CLI.runQuiz(progLangQuiz);
+    }
+} // Main class
